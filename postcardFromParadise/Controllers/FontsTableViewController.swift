48e6a4d9e4dbabf57789e99e433b1fcf279a4fdc
sergioeabarcaf <sergioaf1991@gmail.com>
2018-07-30 07:24:12 -0400

    se agrega un arreglo con las fuentes y de muestran en la celda con su nombre y fuente cargada

--- postcardFromParadise/Controllers/FontsTableViewController.swift
+++ postcardFromParadise/Controllers/FontsTableViewController.swift
@@ -9,15 +9,12 @@
 import UIKit
 
 class FontsTableViewController: UITableViewController {
+    
+    let fonts = UIFont.familyNames.sorted()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,24 +25,23 @@ class FontsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.fonts.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FontCell", for: indexPath)
+        
+        let fontName = self.fonts[indexPath.row]
+        cell.textLabel?.text = fontName
+        cell.textLabel?.font = UIFont(name: fontName, size: 16.0)
 
-        // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
